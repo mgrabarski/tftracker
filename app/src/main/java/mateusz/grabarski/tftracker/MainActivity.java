56f68660f36c04ddc01ff673968a57b7de384c65
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-02-07 20:43:51 +0100

    Checking Google Play Services

--- app/src/main/java/mateusz/grabarski/tftracker/MainActivity.java
+++ app/src/main/java/mateusz/grabarski/tftracker/MainActivity.java
@@ -1,18 +1,22 @@
 package mateusz.grabarski.tftracker;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import mateusz.grabarski.tftracker.services.LocationService;
+import mateusz.grabarski.tftracker.utils.GoogleServiceChecker;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        startService(new Intent(this, LocationService.class));
+        if (GoogleServiceChecker.isGoogleServicesOk(this))
+            startService(new Intent(this, LocationService.class));
     }
 }
