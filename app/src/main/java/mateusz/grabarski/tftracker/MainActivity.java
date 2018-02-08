ce874825a47b02146915e223550d2574c3865de7
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-02-08 19:33:03 +0100

    Checking location permissions

--- app/src/main/java/mateusz/grabarski/tftracker/MainActivity.java
+++ app/src/main/java/mateusz/grabarski/tftracker/MainActivity.java
@@ -4,12 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import mateusz.grabarski.tftracker.services.LocationService;
 import mateusz.grabarski.tftracker.utils.GoogleServiceChecker;
+import mateusz.grabarski.tftracker.utils.Permissions;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final String TAG = "MainActivity";
+
+    private GoogleMap mGoogleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +24,31 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (GoogleServiceChecker.isGoogleServicesOk(this))
-            startService(new Intent(this, LocationService.class));
+            checkLocationPermission();
+    }
+
+    private void checkLocationPermission() {
+        if (Permissions.checkLocationPermission(this))
+            initMap();
+
+        // TODO: 08.02.2018 else consider grand location permission dialog
+    }
+
+    private void initMap() {
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.activity_main_map);
+        mapFragment.getMapAsync(this);
+
+        initLocationService();
+    }
+
+    private void initLocationService() {
+        startService(new Intent(this, LocationService.class));
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
     }
 }
