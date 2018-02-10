fa9fe7961c86661f9df7f0f10072f83af7c23976
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2018-02-10 11:55:04 +0100

    Draw location path

--- app/src/main/java/mateusz/grabarski/tftracker/MainActivity.java
+++ app/src/main/java/mateusz/grabarski/tftracker/MainActivity.java
@@ -12,7 +12,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.squareup.otto.Subscribe;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.inject.Inject;
 
@@ -22,6 +26,7 @@ import mateusz.grabarski.tftracker.base.App;
 import mateusz.grabarski.tftracker.base.AppSettings;
 import mateusz.grabarski.tftracker.base.BaseActivity;
 import mateusz.grabarski.tftracker.base.Constants;
+import mateusz.grabarski.tftracker.data.models.Route;
 import mateusz.grabarski.tftracker.data.models.RouteLocation;
 import mateusz.grabarski.tftracker.services.LocationService;
 import mateusz.grabarski.tftracker.services.events.CurrentLocationEvent;
@@ -101,8 +106,22 @@ public class MainActivity extends BaseActivity implements OnMapReadyCallback {
     }
 
     @Subscribe
-    public void onCurrentRouteLocationUpdate(RouteLocation routeLocation) {
-        Log.d(TAG, "event2: " + routeLocation.toString());
+    public void onCurrentRouteLocationUpdate(Route route) {
+        Log.d(TAG, "event2: " + route.toString());
+
+        refreshPathOnMap(route);
+    }
+
+    private void refreshPathOnMap(Route route) {
+        List<LatLng> locations = new ArrayList<>();
+
+        for (RouteLocation routeLocation : route.getLocations())
+            locations.add(new LatLng(routeLocation.getLatitude(), routeLocation.getLongitude()));
+
+        PolylineOptions polylineOptions = new PolylineOptions();
+        polylineOptions.addAll(locations);
+
+        mGoogleMap.addPolyline(polylineOptions);
     }
 
     private void moveCamera(LatLng latLng, float zoom) {
